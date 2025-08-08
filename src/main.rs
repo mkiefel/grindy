08fd33720cec9a045fa6ce5f2a45c702e7a4b521
Martin Kiefel <me@mkiefel.de>
2025-08-08 20:29:21 +0200

    Add initial version of a small pico program

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+#![no_std]
+#![no_main]
+
+use defmt::*;
+use embassy_executor::Spawner;
+use embassy_time::Timer;
+use {defmt_rtt as _, panic_probe as _};
+
+#[unsafe(link_section = ".bi_entries")]
+#[used]
+pub static PICOTOOL_ENTRIES: [embassy_rp::binary_info::EntryAddr; 4] = [
+    embassy_rp::binary_info::rp_program_name!(c"Grindy"),
+    embassy_rp::binary_info::rp_program_description!(
+        c"Runs a grinder by weight."
+    ),
+    embassy_rp::binary_info::rp_cargo_version!(),
+    embassy_rp::binary_info::rp_program_build_attribute!(),
+];
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    let _p = embassy_rp::init(Default::default());
+
+    loop {
+        info!("howdy");
+        Timer::after_millis(250).await;
+    }
+}
