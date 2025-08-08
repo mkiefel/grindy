68906fcd9b0a00a4502c0b4a391ab2ae9f9fab2a
Martin Kiefel <me@mkiefel.de>
2025-08-08 22:06:58 +0200

    Read load cell

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,11 @@
 
 use defmt::*;
 use embassy_executor::Spawner;
-use embassy_time::Timer;
+use embassy_rp::gpio::{self, Pull};
+use embassy_time::Delay;
+use gpio::{Input, Level, Output};
+use loadcell::{hx711::GainMode, LoadCell};
+use num_traits::float::FloatCore;
 use {defmt_rtt as _, panic_probe as _};
 
 #[unsafe(link_section = ".bi_entries")]
@@ -19,10 +23,33 @@ pub static PICOTOOL_ENTRIES: [embassy_rp::binary_info::EntryAddr; 4] = [
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
-    let _p = embassy_rp::init(Default::default());
+    let p = embassy_rp::init(Default::default());
+    let dt = Input::new(p.PIN_17, Pull::Down);
+    let sck = Output::new(p.PIN_16, Level::Low);
+    let delay = Delay {};
+
+    let mut scale = loadcell::hx711::HX711::new(sck, dt, delay);
+    scale.set_gain_mode(GainMode::A64);
+
+    // The initial tare seems to be a bit off. Just throw it away.
+    scale.tare(10);
+    scale.tare(20);
+    scale.set_scale(1.0);
+    // Get some reference weight and adjust the scale, with something like <reference_weight> /
+    // <values that you get with scaling 1>. E.g.,
+    // scale.set_scale(403.0 / 180919.2);
+
+    let mut buffer = heapless::HistoryBuffer::<_, 25>::new();
 
     loop {
-        info!("howdy");
-        Timer::after_millis(250).await;
+        if scale.is_ready() {
+            let reading = scale.read_scaled().unwrap();
+            buffer.write(reading);
+
+            info!(
+                "Weight {}",
+                ((buffer.iter().sum::<f32>() / (buffer.len() as f32)) * 10.0).floor() / 10.0
+            );
+        }
     }
 }
